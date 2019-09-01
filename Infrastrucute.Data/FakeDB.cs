fab9a67adf27053a6bd103f0893100959a234f24
Tothko <43235034+Tothko@users.noreply.github.com>
2019-09-01 19:08:54 +0200

    "Finished" FakeDB

--- Infrastrucute.Data/FakeDB.cs
+++ Infrastrucute.Data/FakeDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Infrastrucute.Data
@@ -7,16 +8,38 @@ namespace Infrastrucute.Data
     public static class FakeDB
 
     {
-        public static IEnumerable<Pet> Pets;
+        
+        public static List<Pet> Pets;
+        private static Random random = new Random();
+        
+    
 
         public static void InitData()
         {
             for (int i = 0; i < 50; i++)
             {
-                
+                Pets.Add(
+                new Pet
+                {
+                    ID = i,
+                    Name = RandomString(8),
+                    Type = "Dog",
+                    Birthdate = System.DateTime.Now,
+                    SoldDate = System.DateTime.Now,
+                    Color = "Orange",
+                    PreviousOwner = "Marecek",
+                    Price = 500.00
+                });
+                    
             }
+            
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
-       
     }
 }
--- PetShopConsole/Printer.cs
+++ PetShopConsole/Printer.cs
@@ -5,15 +5,25 @@ namespace PetShopConsole
 {
     class Printer
     {
-
+        private static Boolean Run = true;
         public Printer()
         {
             IPetService PS;
         }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+         while(Run)   
+            Console.WriteLine("Welcome to my little Pet shop");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Show all pets");
+            Console.WriteLine("2. Search pets");
+            Console.WriteLine("3. Create new pet");
+            Console.WriteLine("4. Delete pet");
+            Console.WriteLine("5. Update pet");
+            Console.WriteLine("6. Show pets by Price");
+            Console.WriteLine("7.. Show 5 cheapest pets");
+
+
         }
     }
 }
